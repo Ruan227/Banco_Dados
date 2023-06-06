13cd4a29dd0b51f0552a48f8a19447653a4deeb8
Ruan P <ruansilva.pascoal@gmail.com>
2023-06-06 16:19:41 -0300

    espaços

--- models/db.js
+++ models/db.js
@@ -4,6 +4,10 @@
 npm install --save mysql2
 raiz 
 */
+
+
+
+
 <div>PAGINA TEST</div>
 
 const Squelize = require('sequelize')
@@ -11,10 +15,13 @@ const sequelize = new Squelize('gastos_deputados', 'root', 'ruanpascoal227', {
   host: 'localhost',
   dialect: 'mysql'
 });
+
 sequelize.authenticate()
 .then(function(){
     console.log("certo")
-}).catch(function(){
+})
+
+.catch(function(){
     console.log("ERROR")
 })
 module.exports = sequelize;
